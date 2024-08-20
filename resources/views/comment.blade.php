eae6cf2713ea2aa9f3d23aa33510920c534999f0
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-20 23:17:59 +0700

    Comment

---
+++ resources/views/comment.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment</title>
+</head>
+
+<body>
+    <h1>Comment in Laravel Blade</h1>
+    {{--Ini adalah Komentar pada Laravel Blade--}}
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia libero odio exercitationem provident asperiores error, qui aut atque inventore. Libero culpa a harum fugit delectus nobis quod voluptatibus reprehenderit soluta.</p>
+</body>
+
+</html>
---
+++ tests/Feature/CommentTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+
+    public function testComment()
+    {
+        $this->view("comment", [])
+            ->assertSeeText("Comment")
+            ->assertDontSeeText("Komentar")
+            ->assertDontSeeText("pada")
+            ->assertDontSeeText("adalah");
+    }
+}
